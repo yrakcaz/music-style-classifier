5a8353b7b8f73bb7f9cd3f03199f055a6def0884
Zackary Ayoun <ayoun_z@epita.fr>
2014-03-25 03:28:16 +0100

    make clean added

---
+++ teacher.py
@@ -0,0 +1,29 @@
+from songmodel import SongModel
+import json, subprocess
+
+class Teacher:
+    def __init__(self):
+        self.model = SongModel()
+
+    def parse_set(self):
+        content = ""
+        with open("training/set.txt") as f:
+            for l in f:
+                content += l
+        return json.loads(content)
+
+    def train(self):
+        for item in self.parse_set():
+            sub = subprocess.Popen(["sh", "-c", "training/BPMDetector --display " + item[0]], bufsize = 0, stdout = subprocess.PIPE, stdin = subprocess.PIPE)
+            out, err = sub.communicate()
+            i = 0
+            while out[i] != '\n' and out[i] != '.':
+                i += 1
+            val = int(out[:i])
+            self.model.add(item[0], item[1], val)
+            print("ADDED : " + item[0] + " " + item[1] + " " + str(val))
+
+    def print_content(self):
+        vect, mat = self.model.get_datas()
+        print(vect)
+        print(mat)
